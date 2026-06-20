752757f72044ae415221eda018ff5979303e661b
ketanmakwana30 <ketanmakwana0130@gmail.com>
2026-06-20 20:03:28 +0530

    modefy basics

--- 01-Basics-JSX/app.jsx
+++ 01-Basics-JSX/app.jsx
@@ -3,8 +3,11 @@ function App() {
     <>
       <h1>Hello World!</h1>
       <p>This is Basic Component</p>
+      <button>Click Me</button>
     </>
   );
 }
 
+// export default App;
 export { App };
+
--- 01-Basics-JSX/main.jsx
+++ 01-Basics-JSX/main.jsx
@@ -1,4 +1,6 @@
+// import App from './App.jsx'
 import {App} from './App.jsx'
+
 
 createRoot(document.getElementById('root')).render(
     <App />
