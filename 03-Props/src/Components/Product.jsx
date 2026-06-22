cee3763b848cc157d093199c35945da07ed182d0
ketanmakwana30 <ketanmakwana0130@gmail.com>
2026-06-22 07:13:22 +0530

    add props

--- 03-Props/src/Components/Product.jsx
+++ 03-Props/src/Components/Product.jsx
@@ -1,22 +1,28 @@
-import ProductDets from "./ProductDets";
+import ProductDets from "./ProductDets.jsx";
+import MsgBox from "./MsgBox.jsx";
 
 function Product() {
-
-  let feature = ["Hi-tech", "Fast", "Latest"];                                 // Array    
-  let feature2 = {1: "Hi-tech", 2: "Fast", 3: "Latest"}                          // Object
-  let feature3 = [<li>Hi-tect</li>,<li>Fast</li>,<li>Latest</li>];              // Array of element   
+  let feature = ["Hi-tech", "Fast", "Latest"]; // Array
+  let feature2 = { 1: "Hi-tech", 2: "Fast", 3: "Latest" }; // Object
+  let feature3 = [<li>Hi-tect</li>, <li>Fast</li>, <li>Latest</li>]; // Array of element
 
   return (
     <>
-      <ProductDets name="Samsung" features={feature} />
-      <ProductDets name="Vivo V60" features={feature2[1]} />
+      <ProductDets name="Samsung" price={56000} features={feature} />
+      <ProductDets name="Vivo V60" price={83000} features={feature2[1]} />
 
-      <ProductDets name="Radmi" features={["Fast--", "Latest"]} />
-      <ProductDets name="Realme" features={{1: "Hi-tech", 2: "Fast", 3: "Latest"}[3 ] } />
+      <ProductDets name="Radmi" price={16000} features={["Fast--", "Latest"]} />
+      <ProductDets
+        price={23000}
+        name="Realme"
+        features={{ 1: "Hi-tech", 2: "Fast", 3: "Latest" }[3]}
+      />
 
-      <ProductDets name="onePlus" features={feature3}  />
+      <ProductDets name="onePlus" price={43000} features={feature3} />
 
       {/* <ProductDets name="Oppo" features={feature}  />  */}
+
+      <MsgBox userName="Ketan" textColor="red" />
     </>
   );
 }
--- 03-Props/src/Components/ProductDets.jsx
+++ 03-Props/src/Components/ProductDets.jsx
@@ -1,11 +1,17 @@
-function ProductDets({ name, features }) {
-    // const list = features.map((feature) => <li>{feature}</li>);
+function ProductDets({ name, price, features }) {
+  // const list = features.map((feature) => <li>{feature}</li>);
+  const isDiscount = price > 30000;
+  const styles = { backgroundColor: isDiscount ? "#444" : "" };
 
   return (
-    <div className="product">
+    <div className="product" style={styles}>
       <h1>{name}</h1>
-      <p>{features}</p>
-      {/* <p>{list}</p>    */}
+      <div className="details">
+        <p className="price">Price: {price}</p>
+        <p>{features}</p>
+        {/* <p>{list}</p>    */}
+        {isDiscount && <p>5% Descount</p>}
+      </div>
     </div>
   );
 }
